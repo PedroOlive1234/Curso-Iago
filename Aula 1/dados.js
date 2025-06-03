8b1c28556d10a3f75fac44f7a687f5ee7e0d8a45
pedro_olive <pedro.olive15x@gmail.com>
2025-06-02 21:27:48 -0300

    aula 1

---
+++ Aula 1/dados.js
@@ -0,0 +1,29 @@
+const usuario = {
+    id: 1,
+    nome: "João Silva",
+    email: "joao@email.com"
+};
+
+const produto = {
+    id: 101,
+    nome: "Camiseta Polo",
+    preco: 59.90
+};
+
+const pedido = {
+    id: 5001,
+    usuarioId: 1,
+    produtoId: 101
+};
+
+const agendamento = {
+    id: 300,
+    usuarioId: 1,
+    data: "2024-06-10"
+};
+
+const comentario = {
+    id: 900,
+    usuarioId: 1,
+    texto: "Produto de ótima qualidade!"
+}; 
---
+++ Aula 1/index.js
@@ -0,0 +1,25 @@
+const usuario = {
+    nome: null,
+    email: null,
+    senha: null,
+    nome_usuario: null
+};
+
+const produto = {
+    nome: null,
+    preço: null,
+    marca: null,
+    peso: null,
+    cor: null,
+    altura: null,
+    largura: null,
+    profundidade: null,
+    textura: null,
+    material: null,
+    classificação_indicativa: null
+};
+
+const pedidos = {
+    usuario: null,
+    produto: null
+};
